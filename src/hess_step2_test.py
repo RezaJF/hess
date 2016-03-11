3ceb322027fe4e4d4c5cb4f3177ffff44d49440e
huwenboshi <shihuwenbo@ucla.edu>
2016-03-11 12:49:09 -0800

    updated step 2 script

--- src/hess_step2_test.py
+++ src/hess_step2_test.py
@@ -1,72 +1,88 @@
 #!/usr/bin/python
 
-from optparse import OptionParser
+import argparse
 import numpy as np, numpy.linalg
 import os, sys, math
 
-eps = 10**-8
+eps = 10.0**-8.0
 
 # main function
 def main():
    
     # get command line
-    params = get_command_line()
-    prefix = params["prefix"]
-    thres = params["thres"]
-    out_file = params["out_file"]
-    gc = params["gc"]
+    args = get_command_line()
+    prefix = args.prefix
+    num_eig = args.k
+    out_file = args.out
+    gc = args.lambda_gc
+    tot_h2g = args.tot_h2g
 
     # load step1
     locus_info,all_eig,all_prj = load_step1(prefix)
 
-    # get h2g
-    all_h2g,raw_est = get_local_h2g(locus_info, all_eig, all_prj, thres, gc)
-
-    # get variance estimates
-    all_var = get_var_est(locus_info, all_h2g)
+    # estimate h2g jointly when total h2g is not provided
+    if(tot_h2g is None):
+        all_h2g,raw_est = get_local_h2g_no_tot(locus_info, all_eig,
+            all_prj, num_eig, gc)
+        all_var = get_var_est_no_tot(locus_info, all_h2g)
+    # estimate h2g independently when total h2g is provided
+    else:
+        pass
 
     # write output
+    output_local_h2g(out_file, locus_info, raw_est, all_h2g, all_var)
+
+# write to output
+def output_local_h2g(out_file, locus_info, raw_est, all_h2g, all_var):
     out_file = open(out_file, 'w')
-    for i in xrange(len(locus_info)):
-        line = '%s\t%s\t%s\t%s\t%s\t%s\t%d\t%.10f\t%.10f\t%.12f' % (
+    out_file.write('chr\tstart\tend\tnum_snp\tk\tlocal_h2g\tvar\n')
+    num_win = len(locus_info)
+    for i in xrange(num_win):
+        line = '%s\t%s\t%s\t%s\t%d\t%.10f\t%.12f' % (
             locus_info[i][0], locus_info[i][1], locus_info[i][2],
-            locus_info[i][3], locus_info[i][4], locus_info[i][5],
-            raw_est[i][1], raw_est[i][0], all_h2g[i,0], all_var[i,0])
+            locus_info[i][3], raw_est[i][1], all_h2g[i,0], all_var[i,0])
         out_file.write(line+'\n')
     out_file.close()
 
-# compute heritability
-def get_local_h2g(locus_info, all_eig, all_prj, thres, gc):
+# get biased raw estimate
+def get_raw_h2g(locus_info, all_eig, all_prj, max_k, gc):
     raw_est = []
-    all_n = []
     for i in xrange(len(locus_info)):
-        n = float(locus_info[i][5])
-        all_n.append(n)
-        k = min(thres, all_eig[i].size)
+        k = min(max_k, all_eig[i].size)
         tmp = np.divide(all_prj[i][0,0:k], all_eig[i][0,0:k]+eps)
         if(all_eig[i].size > 100):
             raw_est.append((np.sum(tmp)*gc, float(k)))
         else:
             raw_est.append((0.0, 0.0))
+    return raw_est
+    
+# estimate local heritability without total trait h2g
+def get_local_h2g_no_tot(locus_info, all_eig, all_prj, num_eig, gc):
+    
+    # get step 1 tot
     num_win = len(locus_info)
-    A = np.matrix(np.zeros((num_win, num_win)))
-    b = np.matrix(np.zeros((num_win, 1)))
+    raw_est = get_raw_h2g(locus_info, all_eig, all_prj, num_eig, gc)
+    tot_step1 = 0.0
     for i in xrange(num_win):
         n = float(locus_info[i][5])
-        for j in xrange(num_win):
-            if(i == j):
-                A[i,j] = n-raw_est[j][1]
-            else:
-                A[i,j] = -raw_est[j][1]
-        b[i,0] = n*raw_est[i][0]-raw_est[i][1]
-    mean_n = np.mean(np.array(all_n))
-    A = A/(mean_n+eps)
-    b = b/(mean_n+eps)
-    est = np.linalg.pinv(A)*b
+        tot_step1 += raw_est[i][0]-raw_est[i][1]/(n+eps)
+
+    print tot_step1
+
+    # get step 2 estimate
+    est = []
+    for i in xrange(num_win):
+        n = float(locus_info[i][5])
+        local_h2g = raw_est[i][0]
+        local_h2g -= (1.0-tot_step1)*raw_est[i][1]/(n+eps)
+        est.append(local_h2g)
+    est = np.matrix(est)
+    print np.sum(est)
+    sys.exit()
     return est,raw_est
 
-# est var
-def get_var_est(locus_info, all_h2g):
+# estimate variance when local heritability is estimated jointly
+def get_var_est_no_tot(locus_info, all_h2g):
     num_win = len(locus_info)
     tot = np.sum(all_h2g)
     A = np.matrix(np.zeros((num_win, num_win)))
@@ -134,37 +150,19 @@ def load_step1(prefix):
 
 # get command line
 def get_command_line():
-    
-    # get command line
-    parser = OptionParser(add_help_option=False)
-    parser.add_option("-p", "--prefix", dest="prefix")
-    parser.add_option("-t", "--threshold", type="int",
-        dest="thres", default=30)
-    parser.add_option("-o", "--out_file", dest="out_file")
-    parser.add_option("-g", "--gc", type="float", dest="gc",
-        default=1.0)
-    (options, args) = parser.parse_args()
-    prefix = options.prefix
-    thres = options.thres
-    out_file = options.out_file
-    gc = options.gc
-
-    # check command line
-    if(prefix == None or thres == None or out_file == None):
-        sys.stderr.write("Usage:\n")
-        sys.stderr.write("\tUse -p to specify prefix\n")
-        sys.stderr.write("\tUse -t to specify threshold\n")
-        sys.stderr.write("\tUse -o to specify output file\n")
-        sys.exit()
-    
-    # create param dictionary
-    params = dict()
-    params["prefix"] = prefix
-    params["thres"] = int(thres)
-    params["out_file"] = out_file
-    params["gc"] = float(gc)
-
-    return params
+    parser = argparse.ArgumentParser(description='Estimate local h2g')
+    parser.add_argument('--prefix', dest='prefix', type=str,
+                   help='Prefix used for step 1', required=True)
+    parser.add_argument('--out', dest='out', type=str,
+                   help='Output file name', required=True)
+    parser.add_argument('--k', dest='k', type=int, default=50,
+                   help='Maximum number of eigenvectors to use (default 50)')
+    parser.add_argument('--lambda_gc', dest='lambda_gc', type=float,
+                   default=1.0, help='Genomic control factor (default 1.0)')
+    parser.add_argument('--tot_h2g', dest='tot_h2g', type=float,
+                   help='Total trait SNP heritability')
+    args = parser.parse_args()
+    return args
 
 if(__name__ == '__main__'):
     main()
